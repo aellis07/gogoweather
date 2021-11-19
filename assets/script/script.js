d118437e1054e98b5375f19006c741d8b8f55326
Anthony Ellis <amellis245@gmail.com>
2021-11-19 10:03:08 -0500

    created current weather api and passed api key and request url through fetch request

--- assets/script/script.js
+++ assets/script/script.js
@@ -24,3 +24,16 @@ document
         getCurrentWeather(search);
         getWeatherForecast(search);
     });
+
+// ============================= FUNCTIONS-APIs
+
+// OPENWEATHER.ORG
+// CURRENT WEATHER
+function getCurrentWeather(search) {
+    var api_key = "092e71a1090dac60f74b32dccbf7f5bc";
+    var requestUrl =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        search.value +
+        "&units=imperial&appid=";
+    fetch(requestUrl + api_key);
+}
