29706bad3f7e7715b9291075f640e8acfb904a98
Anthony Ellis <amellis245@gmail.com>
2021-11-19 10:08:11 -0500

    passed api data to html objects for 5day forecast

--- assets/script/script.js
+++ assets/script/script.js
@@ -74,6 +74,90 @@ function getWeatherForecast(search) {
         })
         .then((data) => {
             console.log(data);
+            // ======================================
+            // DAY 1
+
+            const rawDate1 = data.list[0].dt_txt;
+            const newDate1 = rawDate1.substr(0, 11);
+            console.log(newDate1);
+            // console.log(data.list[0].dt_txt);
+
+            document.getElementById("dateOne").textContent =
+                "Date: " + newDate1;
+            document.getElementById("tempOne").textContent =
+                "Temperature: " + Math.round(data.list[0].main.temp) + " °F";
+            document.getElementById("windOne").textContent =
+                "Wind Speed: " + Math.round(data.list[0].wind.speed) + " mph";
+            document.getElementById("humidityOne").textContent =
+                "Humidity: " + Math.round(data.list[0].main.humidity) + " %";
+
+            // ======================================
+            // DAY 2
+
+            const rawDate2 = data.list[8].dt_txt;
+            const newDate2 = rawDate2.substr(0, 11);
+            console.log(newDate2);
+            // console.log(data.list[8].dt_txt);
+
+            document.getElementById("dateTwo").textContent =
+                "Date: " + newDate2;
+            document.getElementById("tempTwo").textContent =
+                "Temperature: " + Math.round(data.list[8].main.temp) + " °F";
+            document.getElementById("windTwo").textContent =
+                "Wind Speed: " + Math.round(data.list[8].wind.speed) + " mph";
+            document.getElementById("humidityTwo").textContent =
+                "Humidity: " + Math.round(data.list[8].main.humidity) + " %";
+
+            // ======================================
+            // DAY 3
+
+            const rawDate3 = data.list[16].dt_txt;
+            const newDate3 = rawDate3.substr(0, 11);
+            console.log(newDate3);
+            // console.log(data.list[16].dt_txt);
+
+            document.getElementById("dateThree").textContent =
+                "Date: " + newDate3;
+            document.getElementById("tempThree").textContent =
+                "Temperature: " + Math.round(data.list[16].main.temp) + " °F";
+            document.getElementById("windThree").textContent =
+                "Wind Speed: " + Math.round(data.list[16].wind.speed) + " mph";
+            document.getElementById("humidityThree").textContent =
+                "Humidity: " + Math.round(data.list[16].main.humidity) + " %";
+
+            // ======================================
+            // DAY 4
+
+            const rawDate4 = data.list[24].dt_txt;
+            const newDate4 = rawDate4.substr(0, 11);
+            console.log(newDate4);
+            // console.log(data.list[24].dt_txt);
+
+            document.getElementById("dateFour").textContent =
+                "Date: " + newDate4;
+            document.getElementById("tempFour").textContent =
+                "Temperature: " + Math.round(data.list[24].main.temp) + " °F";
+            document.getElementById("windFour").textContent =
+                "Wind Speed: " + Math.round(data.list[24].wind.speed) + " mph";
+            document.getElementById("humidityFour").textContent =
+                "Humidity: " + Math.round(data.list[24].main.humidity) + " %";
+
+            // ======================================
+            // DAY 5
+
+            const rawDate5 = data.list[32].dt_txt;
+            const newDate5 = rawDate5.substr(0, 11);
+            console.log(newDate5);
+            // console.log(data.list[32].dt_txt);
+
+            document.getElementById("dateFive").textContent =
+                "Date: " + newDate5;
+            document.getElementById("tempFive").textContent =
+                "Temperature: " + Math.round(data.list[32].main.temp) + " °F";
+            document.getElementById("windFive").textContent =
+                "Wind Speed: " + Math.round(data.list[32].wind.speed) + " mph";
+            document.getElementById("humidityFive").textContent =
+                "Humidity: " + Math.round(data.list[32].main.humidity) + " %";
         });
 }
 
